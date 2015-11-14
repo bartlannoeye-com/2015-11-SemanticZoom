fbe71b1cbda4e42e2148aa81951c6739b25d8884
bartlannoeye <bart.lannoeye@gmail.com>
2015-11-15 00:17:31 +0100

    Initial code checkin

---
+++ src/SemanticZoom/SemanticZoom/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace SemanticZoom
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var collectionGroups = Collection.View.CollectionGroups;
+            ((ListViewBase)this.SemanticZoom.ZoomedOutView).ItemsSource = collectionGroups;
+        }
+    }
+}
