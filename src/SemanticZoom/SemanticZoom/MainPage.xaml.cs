0809f7588593b14ffe042ab97dfe807c398b0974
Depechie <glenn.versweyveld@gmail.com>
2015-11-16 22:10:16 +0100

    Quick fix for triggering visual state when data is loaded

--- src/SemanticZoom/SemanticZoom/MainPage.xaml.cs
+++ src/SemanticZoom/SemanticZoom/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
 namespace SemanticZoom
@@ -11,6 +12,13 @@ namespace SemanticZoom
         public MainPage()
         {
             this.InitializeComponent();
+            this.ZoomedInGridView.Loaded += ZoomedInGridView_Loaded;
+        }
+
+        private void ZoomedInGridView_Loaded(object sender, RoutedEventArgs e)
+        {
+            //TODO: Only go to the correct state in reference to the windows size! For test we set it fixed...
+            VisualStateManager.GoToState(this, "VisualStateMin320", false);
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
